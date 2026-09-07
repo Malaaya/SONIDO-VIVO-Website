ed7c437ab3da12fb6f48af3c2acb05a579601ce9
Malaya <angelkraus4@gmail.com>
2026-09-07 18:46:58 -0300

    Añadido de registro.html y cambios en index

---
+++ js/registro.js
@@ -0,0 +1,20 @@
+const comunasPorRegion = {
+    "Valparaíso": [
+      "Viña del Mar",
+      "Valparaíso",
+      "Quilpué",
+      "Villa Alemana",
+      "Concón",
+      "Quillota"
+    ],
+    "Metropolitana": [
+      "Santiago",
+      "Providencia",
+      "Las Condes",
+      "Ñuñoa",
+      "Independencia",
+      "San Joaquín",
+      "Maipú",
+      "La Florida"
+    ]
+  };
